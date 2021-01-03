db6d53233d858a360d90e83f85e7ce10b44159d7
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-01-02 18:28:10 -0800

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCgoVgK86WaxAOdxsz2-bTFelaPm8BQUGc",
+  authDomain: "superchat-abe1c.firebaseapp.com",
+  projectId: "superchat-abe1c",
+  storageBucket: "superchat-abe1c.appspot.com",
+  messagingSenderId: "158005517033",
+  appId: "1:158005517033:web:bd76294a3f38c105f86e78",
+  measurementId: "G-WH1CQHQVRS"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
