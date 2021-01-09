620edbb944cf92ff1e27311495f5d9c2ebdc1dae
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-01-09 12:38:08 -0800

    Footer Completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,25 @@ import './Footer.css'
 const Footer = () => {
     return (
         <Fragment>
-            <h1>Footer</h1>
+            <section className="footer-container">
+                <div className="messages">
+                    <p className="copyright">SecretChat © 2020</p>
+                </div>
+                <div className="social-icons">
+                    <a href="https://www.instagram.com/">
+                        <i className="fab fa-instagram" />
+                    </a>
+                    <a href="https://twitter.com/">
+                        <i className="fab fa-twitter" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/bilal-abraham-3140491b8/">
+                        <i className="fab fa-linkedin" />
+                    </a>
+                    <a href="https://github.com/bilal-abraham/secretchat">
+                        <i className="fab fa-github" />
+                    </a>
+                </div>
+            </section>
         </Fragment>
     )
 }
