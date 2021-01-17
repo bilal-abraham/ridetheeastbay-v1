efb7e5e326bfa92b0396d9a6bdd2aced9df9ae3c
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-01-16 21:06:24 -0800

    donate page finished

---
+++ src/components/DonateHero.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+import './DonateHero.css'
+
+const DonateHero = () => {
+    return (
+        <Fragment>
+            <div className="hero-container">
+                <div className="donate-wraper" />
+                <h1>Donate on to our Initiative</h1>
+                <p>Direct Message Us on Instagram</p>
+                <a href="https://www.instagram.com/ridetheeastbay/?igshid=lm061psb2uzw">
+                    <button className="btn">
+                        DONATE
+                    </button>
+                </a>
+            </div>
+        </Fragment>
+    )
+}
+
+export default DonateHero
--- src/components/pages/Donate.js
+++ src/components/pages/Donate.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import Navbar from '../Navbar'
+import DonateHero from '../DonateHero'
 import Footer from '../Footer'
 
 const Donate = () => {
     return (
         <Fragment>
             <Navbar />
+            <DonateHero />
             <Footer />
         </Fragment>
     )
