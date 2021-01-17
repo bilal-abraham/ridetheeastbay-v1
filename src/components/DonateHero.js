00859567c2862cefdd0406ec6f7ba66a91d25f90
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-01-16 22:17:09 -0800

    donate p tag edit

--- src/components/DonateHero.js
+++ src/components/DonateHero.js
@@ -7,7 +7,7 @@ const DonateHero = () => {
             <div className="hero-container">
                 <div className="donate-wraper" />
                 <h1>Donate to our Initiative</h1>
-                <p>Direct Message Us on Instagram</p>
+                <p>Message us on Instagram or Email</p>
                 <a href="https://www.instagram.com/ridetheeastbay/?igshid=lm061psb2uzw">
                     <button className="btn">
                         DONATE<i className="fas fa-donate" />
