a487065753ac58bed69976b683fdd9001ed062f9
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-02-02 21:10:09 -0800

    donate link open new tab form

--- src/components/DonateHero.js
+++ src/components/DonateHero.js
@@ -2,17 +2,29 @@ import React, { Fragment } from 'react'
 import './DonateHero.css'
 
 const DonateHero = () => {
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
     return (
         <Fragment>
             <div className="hero-container">
-                <div className="donate-wraper" />
+                <div className="donate-wrapper" />
                 <h1>Donate to our Initiative</h1>
                 <p>Message us on Instagram or Email</p>
-                <a href="https://www.paypal.com/paypalme/ridetheeastbay">
-                    <button className="btn">
-                        DONATE<i className="fas fa-donate" />
-                    </button>
-                </a>
+                <div className="donation-inline">
+                    <div onClick={() => openInNewTab('https://www.paypal.com/paypalme/ridetheeastbay')}>
+                        <button className="btn">
+                            DONATE<i className="fas fa-donate" />
+                        </button>
+                    </div>
+                    <div className="donate-btn-spacer" />
+                    <div onClick={() => openInNewTab('https://docs.google.com/forms/d/e/1FAIpQLSdQgLPfFF-6bucteUCHSQ9jI1AVAynPDyETGo_a9gH1Okw6fA/viewform?usp=sf_link')}>
+                        <button className="btn">
+                            DONATE<i className="fas fa-bicycle" />
+                        </button>
+                    </div>
+                </div>
             </div>
         </Fragment>
     )
