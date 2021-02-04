a6ed8d31eaafc2e104582e77dc94c45a56ec505c
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-02-04 15:11:24 -0800

    availible bikes, and donated bikes updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,62 +10,38 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                            src="images/img-bike.png"
-                            text="Description #1"
+                            src="images/bike-1.png"
+                            text="Red Royal Baby Bike (14in Tire)"
                             label="Kids"
                         />
                         <CardItem
-                            src="images/img-bike.png"
-                            text="Description #2"
+                            src="images/bike-8.png"
+                            text="Green KaZam Walker Bike (12in Tire)"
                             label="Kids"
-                        />
-                        <CardItem
-                            src="images/img-bike.png"
-                            text="Description #3"
-                            label="Adult"
                         />
                     </ul>
                     <ul className="cards__items">
                         <CardItem
-                            src="images/img-bike.png"
-                            text="Description #1"
+                            src="images/bike-9.png"
+                            text="Light Blue Electra Hawaii Bike (16in Tire)"
                             label="Kids"
                         />
                         <CardItem
-                            src="images/img-bike.png"
-                            text="Description #2"
+                            src="images/bike-10.png"
+                            text="Purple Specialized Bike (20in Tire)"
                             label="Kids"
                         />
                     </ul>
                 </div>
+            </div>
+            <h1>Donated Bikes</h1>
+            <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                            src="images/img-bike.png"
-                            text="Description #1"
-                            label="Kids"
-                        />
-                        <CardItem
-                            src="images/img-bike.png"
-                            text="Description #2"
-                            label="Kids"
-                        />
-                        <CardItem
-                            src="images/img-bike.png"
-                            text="Description #3"
-                            label="Adult"
-                        />
-                    </ul>
-                    <ul className="cards__items">
-                        <CardItem
-                            src="images/img-bike.png"
-                            text="Description #3"
-                            label="Adult"
-                        />
-                        <CardItem
-                            src="images/img-bike.png"
-                            text="Description #2"
-                            label="Kids"
+                            src="images/bike-2.png"
+                            text="Blue Raleigh Bike (16in Tire)"
+                            label="Donated!"
                         />
                     </ul>
                 </div>
