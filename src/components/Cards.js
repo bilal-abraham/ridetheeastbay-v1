031a3cb91d1e5bb13e62481ca16368f3616dccca
Bilal Abraham <bilal.abraham.17@gmail.com>
2021-02-16 20:47:49 -0800

    re adjustments on grid

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,6 +10,11 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
+                            src="images/bike-1.png"
+                            text="Red Royal Baby Bike (14 Inch Tire)"
+                            label="Kids"
+                        />
+                        <CardItem
                             src="images/bike-3.png"
                             text="Green KaZam Walker Bike (12 Inch Tire)"
                             label="Kids"
@@ -19,33 +24,28 @@ function Cards() {
                             text="Light Blue Electra Hawaii Bike (16 Inch Tire)"
                             label="Kids"
                         />
+                    </ul>
+                    <ul className="cards__items">
                         <CardItem
                             src="images/bike-5.png"
                             text="Purple Specialized Bike (20 Inch Tire)"
                             label="Kids"
                         />
-                    </ul>
-                    <ul className="cards__items">
                         <CardItem
                             src="images/bike-6.png"
                             text="Honda Racing Kids Mountain Bike (20 Inch Tire)"
                             label="Kids"
                         />
+                    </ul>
+                    <ul className="cards__items">
                         <CardItem
                             src="images/bike-7.png"
                             text="Black Upland Kids Bike (12 Inch Tire)"
                             label="Kids"
                         />
-                    </ul>
-                    <ul className="cards__items">
                         <CardItem
                             src="images/bike-8.png"
                             text="Pink Trek Kids Bike (16 Inch Tire)"
-                            label="Kids"
-                        />
-                        <CardItem
-                            src="images/bike-9.png"
-                            text="Purple Huffy Kids Bike (14 Inch Tire)"
                             label="Kids"
                         />
                         <CardItem
@@ -66,8 +66,8 @@ function Cards() {
                             label="Donated!"
                         />
                         <CardItem
-                            src="images/bike-1.png"
-                            text="Red Royal Baby Bike (14 Inch Tire)"
+                            src="images/bike-9.png"
+                            text="Purple Huffy Kids Bike (14 Inch Tire)"
                             label="Donated!"
                         />
                     </ul>
